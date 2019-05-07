81f7f4fe05acd47617e225518d77a27fd7253547
Luis Marques <ei11159@fe.up.pt>
2019-05-07 16:52:08 +0100

    more scratches

--- Project2/server.c
+++ Project2/server.c
@@ -14,29 +14,56 @@
 #include <semaphore.h>
 
 #include "sope.h"
-#include "types.h"
 
-int fifo_fd;
+bank_account_t bank_accounts[MAX_BANK_ACCOUNTS];
 
-int main(int argc, char *argv[]){
-    umask(0000);
-    int num_threads = atoi(argv[1]);
-    if(num_threads > MAX_BANK_OFFICES){
-        printf("Numero maximo de balcões e' %d\n", MAX_BANK_OFFICES);
+int srv_fifo_fd;
+
+int num_threads;
+char *admin_pass = "";
+
+void openServerFIFO(){
+    if(mkfifo(SERVER_FIFO_PATH, FIFO_RW_MODE) != 0){
+        printf("%s nao foi possivel abrir\n", SERVER_FIFO_PATH);
         exit(1);
     }
-    mkfifo(SERVER_FIFO_PATH, 0777);
 
-    fifo_fd = open(SERVER_FIFO_PATH, O_RDONLY);
+    srv_fifo_fd = open(SERVER_FIFO_PATH, O_RDONLY);
+}
+
+void *balcaoEletronico(void *arg){
+
+}
+
+
+int main(int argc, char *argv[]){
+    if(argc > 3){
+        printf("%s precisa de 2 argumentos", argv[0]);
+        exit(1);
+
+    }
+    mode_t old_mask = umask(NEW_MASK);
+
+    num_threads = atoi(argv[1]);
+    if(num_threads > MAX_BANK_OFFICES){
+        printf("O maximo permitido e' %d\n", MAX_BANK_OFFICES);
+        exit(2);
+    }
+    admin_pass = argv[2];
+    if(strlen(admin_pass) < MIN_PASSWORD_LEN || strlen(admin_pass) > MAX_PASSWORD_LEN){
+        printf("Insira uma password entre %d e %d caracteres\n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
+        exit(3);
+    }
 
     pthread_t thread [num_threads];
 
     for(int i = 0; i < num_threads; i++){
-        if(pthread_create(&thread[i],NULL,,) != 0){
-
+        if(pthread_create(&thread[i],NULL,balcaoEletronico, (void *) &bank_accounts[i]) != 0){
+            exit(4);
         }
-        logBankOfficeOpen(STDOUT_FILENO,MAIN_THREAD_ID,)
     }
+
+    umask(old_mask);
 
     return 0;
 }
--- Project2/user.c
+++ Project2/user.c
@@ -14,17 +14,114 @@
 #include <semaphore.h>
 
 #include "sope.h"
-#include "types.h"
 
-int fifo_fd;
+int srv_fifo_fd;
+int user_fifo_fd;
+char *user_fifo;
+
+int user_id;
+char *pass = "";
+int delay;
+int op;
+char *arg = "";
+
+op_type_t op_type;
+
+req_header_t req_header;
+
+req_create_account_t req_create_account;
+req_transfer_t req_transfer;
+
+
+void openServerFIFO(){
+    if(mkfifo(SERVER_FIFO_PATH, FIFO_RW_MODE) != 0){
+        printf("%s nao foi possivel abrir\n", SERVER_FIFO_PATH);
+        exit(1);
+    }
+
+    srv_fifo_fd = open(SERVER_FIFO_PATH, O_WRONLY);
+}
+
+void openUserFIFO(){
+    if(mkfifo(user_fifo, FIFO_RW_MODE) != 0){
+        printf("%s nao foi possivel abrir\n", user_fifo);
+        exit(2);
+    }
+
+    user_fifo_fd = open(user_fifo, O_RDONLY);
+}
+
+void getUserFIFOName(){
+    char *pid_buf = malloc(sizeof(WIDTH_ID));
+
+    pid_t pid = getpid();
+
+    sprintf(pid_buf, "%*d",WIDTH_ID,pid);
+
+    user_fifo = malloc(USER_FIFO_PATH_LEN);
+
+    strcpy(user_fifo, USER_FIFO_PATH_PREFIX);
+    strcat(user_fifo, pid_buf);
+}
+
+void adminAcess(){
+    write(srv_fifo_fd, &user_id,sizeof(int));
+    write(srv_fifo_fd, pass, sizeof(char *) * strlen(pass));
+    if(op == 0){
+        write(srv_fifo_fd, arg, sizeof(char *) * strlen(arg));
+    }
+    else if(op == 3){
+    }
+}
+
+void userAcess(){
+
+}
 
 int main(int argc, char *argv[]){
-    umask(0000);
-    mkfifo(SERVER_FIFO_PATH, 0777);
+    if(argc < 5 || argc > 5){
+        printf("%s precisa de 5 argumentos\n", argv[0]);
+        exit(1);
+    }
 
-    fifo_fd = open(SERVER_FIFO_PATH, O_WRONLY);
+    mode_t old_mask = umask(NEW_MASK);
 
-    mkfifo()
+    user_id = atoi(argv[1]);
+    if(user_id > MAX_BANK_ACCOUNTS){
+        printf("Numero maximo de contas ultrapassado, limite: %d", MAX_BANK_ACCOUNTS);
+        exit(2);
+    }
+    pass = argv[2];
+    if(strlen(pass) < MIN_PASSWORD_LEN || strlen(pass) > MAX_PASSWORD_LEN){
+        printf("Insira uma password entre %s e %s\n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
+        exit(3);
+    }
+    delay = atoi(argv[3]);
+    if(delay > MAX_OP_DELAY_MS){
+        printf("Atraso de operacao deve ser inferior a %d", MAX_OP_DELAY_MS);
+        exit(4);
+    }
+    op_type = atoi(argv[4]);
+    if(op_type < 0 || op_type > 3){
+        printf("Codigo de operacao entre 0 e 3\n");
+        exit(5);
+    }
+    arg = argv[5];
+
+    getUserFIFOName();
+
+
+    if(user_id == ADMIN_ACCOUNT_ID){
+        adminAcess();
+    }
+    else {
+        userAcess();
+    }
+
+
+
+
+    umask(old_mask);
 
     return 0;
 }
